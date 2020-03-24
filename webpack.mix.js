7889a7bb4245c201849728e1e76f7ba92c323fc4
cbl <lennart.carbe@gmail.com>
2020-03-24 19:22:17 +0100

    webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require("laravel-mix");
+const fs = require('fs');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +11,44 @@ const mix = require("laravel-mix");
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+ class MergePackageJson {
+     /**
+      * The API name for the component.
+      */
+     name() {
+         return ['mergePackageJson', 'package'];
+     }
+
+     /**
+      * Register the component.
+      *
+      * @param {*} from
+      * @param {string} to
+      */
+     register(from, to) {
+         console.log(from);
+         let self = this;
+         self.onChange(from, to);
+         fs.watchFile(from, (curr, prev) => {
+             self.onChange(from, to);
+         });
+     }
+
+     onChange(from, to) {
+         let toPath = to + '/package.json'
+         let fromJson = JSON.parse(fs.readFileSync(from))
+         let toJson = JSON.parse(fs.readFileSync(toPath))
+         toJson.devDependencies = fromJson.devDependencies
+         toJson.dependencies = fromJson.dependencies
+         delete toJson.dependencies['fjord']
+
+         let spacing = 4
+         fs.writeFile(toPath, JSON.stringify(toJson, null, spacing), (err) => {});
+     }
+ }
+
+mix.extend('package', new MergePackageJson());
 
 mix.webpackConfig({
     resolve: {
@@ -26,7 +65,14 @@ mix.webpackConfig({
     }
 });
 
-mix.js("resources/js/app.js", "public/fjord/js").browserSync({
-    proxy: "fjord-playground.aw",
-    notify: false
-});
+mix.js('resources/js/app.js', 'public/fjord/js')
+    .sass(
+        'packages/aw-studio/fjord/resources/sass/app.scss',
+        'public/fjord/css'
+    )
+    .copyDirectory('public/fjord', 'packages/aw-studio/fjord/publish/assets')
+    .package('package.json', 'packages/aw-studio/fjord')
+    .browserSync({
+        proxy: "fjord-dev.aw",
+        notify: false
+    });
